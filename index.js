3639b315ab326b23c2479449e987907d00c87af4
migooovr <49065741+migooovr@users.noreply.github.com>
2019-04-06 00:01:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -111,41 +111,6 @@ message.reply(' ``Page de la commande _help ! \n _ban sert à bannir des joueurs
         }
 
       
-        
-        if(command === "_mute"){
-
-          if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'avez pas les droits pour muter un utilisateur !");
-      
-          let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-          if(!toMute) return message.channel.send("Merci d'entrer un utilisateur !");
-          let role = message.guild.roles.find(r => r.name === "Utilisateurs mutés");
-          if(!role){
-            try {
-              role = await message.guild.createRole({
-                name: "Utilisateurs mutés",
-                color:"#000000",
-                permissions:[]
-              });
-      
-              message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(role, {
-                  SEND_MESSAGES: false,
-                  ADD_REACTIONS: false
-                });
-              });
-            } catch (e) {
-              console.log(e.stack)
-            }
-          }
-      
-          if(toMute.roles.has(role.id)) return message.channel.send('Cet utilisateur est déjà muté !');
-      
-          await(toMute.addRole(role));
-          message.channel.send("Je l'ai muté !");
-      
-          return;
-        }
-      
 
 
         if(command === "_purge") {
